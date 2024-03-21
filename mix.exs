e5b6c5a271bf191393275504eed968388bc43c4f
teddy <68400183+TC751@users.noreply.github.com>
2024-03-21 14:03:06 +0200

    added dependencies

--- mix.exs
+++ mix.exs
@@ -5,8 +5,9 @@ defmodule Zrl.MixProject do
     [
       app: :zrl,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
@@ -19,7 +20,7 @@ defmodule Zrl.MixProject do
   def application do
     [
       mod: {Zrl.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :logger_file_backend]
     ]
   end
 
@@ -32,26 +33,37 @@ defmodule Zrl.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.7.10"},
-      {:phoenix_ecto, "~> 4.4"},
-      {:ecto_sql, "~> 3.10"},
-      {:postgrex, ">= 0.0.0"},
+      {:phoenix, "~> 1.5.7"},
+      {:phoenix_ecto, "~> 4.1"},
+      {:ecto_sql, "~> 3.4"},
       {:tds, ">= 0.0.0"},
-      {:phoenix_html, "~> 3.3"},
+      {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.20.1"},
-      {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.8.2"},
-      {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
-      {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
-      {:swoosh, "~> 1.3"},
-      {:finch, "~> 0.13"},
-      {:telemetry_metrics, "~> 0.6"},
-      {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.20"},
-      {:jason, "~> 1.2"},
-      {:dns_cluster, "~> 0.1.1"},
-      {:plug_cowboy, "~> 2.5"}
+      {:phoenix_live_dashboard, "~> 0.4"},
+      {:telemetry_metrics, "~> 0.4"},
+      {:telemetry_poller, "~> 0.4"},
+      {:gettext, "~> 0.11"},
+      {:jason, "~> 1.0"},
+      {:plug_cowboy, "~> 2.0"},
+      {:cachex, "~> 2.0"},
+      {:timex, "~> 3.7"},
+      {:atomic_map, "~> 0.8"},
+      {:pipe_to, "~> 0.2"},
+      {:bamboo, "~> 1.3"},
+      {:bamboo_smtp, "~> 2.1.0"},
+      {:decimal, "~> 1.9"},
+      {:scrivener, "~> 2.0"},
+      {:scrivener_ecto, "~> 2.7", override: true},
+      {:quantum, "~> 2.2.7"},
+      {:poison, "~> 3.1"},
+      {:logger_file_backend, "~> 0.0.10"},
+      {:pdf_generator, "~> 0.6.2"},
+      {:bbmustache, github: "soranoba/bbmustache"},
+      {:elixlsx, "~> 0.4.2"},
+      {:number, "~> 0.5.7"},
+      {:xlsxir, "~> 1.6.2"},
+      {:csv, "~>2.3"},
+      {:endon, "~> 1.0"}
     ]
   end
 
@@ -63,13 +75,10 @@ defmodule Zrl.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
+      setup: ["deps.get", "ecto.setup", "cmd npm install --prefix assets"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
-      "assets.build": ["tailwind default", "esbuild default"],
-      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
     ]
   end
 end
