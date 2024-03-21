ea0722e03efe526c9709b8e5733c9c6b9b5af6a6
teddy <68400183+TC751@users.noreply.github.com>
2024-03-21 09:05:44 +0200

    table migrtions

---
+++ priv/repo/migrations/20210610084916_create_tbl_defects.exs
@@ -0,0 +1,14 @@
+defmodule Zrl.Repo.Migrations.CreateTblDefects do
+  use Ecto.Migration
+
+  def change do
+    create table(:tbl_defects) do
+      add :code, :string
+      add :description, :string
+
+      timestamps()
+    end
+
+    create unique_index(:tbl_defects, [:description])
+  end
+end
